5908469a357edc467d4ff7cd3f4c99dca0faeb9a
anil-oommen <aniloommen13@yahoo.com>
2018-03-22 13:04:42 +0800

    Mongodb connect with username pwd

--- src/main/java/com/oom/hive/central/MongoConfig.java
+++ src/main/java/com/oom/hive/central/MongoConfig.java
@@ -3,6 +3,10 @@ package com.oom.hive.central;
 
 import com.mongodb.MongoClient;
 //import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +14,14 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @Configuration
 public class MongoConfig {
+
+    private static final org.slf4j.Logger logger =
+            LoggerFactory.getLogger(MongoConfig.class);
+
 
     @Value( "${app.mongodb.hostname}" )
     String hostname;
@@ -31,7 +40,29 @@ public class MongoConfig {
 
     @Bean
     public MongoTemplate mongoTemplate() {
-        MongoClient mongoClient = new MongoClient(hostname,port);
+
+        MongoClient mongoClient;
+
+        if(StringUtils.isEmpty(username) || StringUtils.isEmpty(password)){
+            logger.warn("No UserName or Password set. Will attempt connecting directly");
+            mongoClient = new MongoClient(hostname,port);
+        }else{
+            logger.warn("Connecting with username: " + username + " password:XXXXXX");
+            MongoCredential mongodbConnectCreds =
+                    MongoCredential.createCredential(
+                            username,
+                            database,
+                            password.toCharArray()
+                    );
+            ServerAddress mongoServerAddress =
+                    new ServerAddress(hostname, port);
+            mongoClient = new MongoClient(
+                    mongoServerAddress,
+                    Arrays.asList(mongodbConnectCreds)
+            );
+        }
+
+
         MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, database);
         return mongoTemplate;
     }
